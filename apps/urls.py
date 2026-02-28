2e59a0458ba0bca64a152272e94b2e336c1a9443
maxsudbekjon <toshpulatovm73@gmail.com>
2026-02-28 09:28:46 +0500

    fixed bug

--- apps/urls.py
+++ apps/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns =[
     path('teachers/', include('apps.teacher.urls')),
-    path('lead/',include('lead.urls'))
+    path('lead/',include('apps.lead.urls'))
 ]
