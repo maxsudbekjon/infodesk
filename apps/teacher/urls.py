1023c73ea26ed28d43a004b08554f8bb7aeb5567
abayaxmedov <ahmedovabay@gmail.com>
2026-03-02 16:32:30 +0500

    extend schema in teachers app

--- apps/teacher/urls.py
+++ apps/teacher/urls.py
@@ -1,14 +1,3 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-# from .views import TeacherViewSet
-#
-# router = DefaultRouter()
-# router.register(r'teachers', TeacherViewSet, basename='teacher')
-#
-# urlpatterns = [
-#     path('api/', include(router.urls)),
-# ]
-
 from django.urls import path
 from .views import (
     # TeacherListCreateAPIView,
