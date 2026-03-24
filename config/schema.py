e6be4c44d36b88da39154ff9860576229ba57238
Maxsud Toshpulat <maxsudtoshpulat@Maxsuds-MacBook-Air.local>
2026-03-24 18:25:27 +0500

    a

--- config/schema.py
+++ config/schema.py
@@ -6,6 +6,17 @@ ALLOWED_SCHEMA_PATHS = {
     "/apps/group/group-studnets-attendance/{id}",
     "/apps/group/group-students-attendance/{id}",
     "/apps/group/group-students-grades/{id}",
+    "/apps/teachers/my-groups/",
+    "/apps/teachers/my-courses/",
+    "/apps/teachers/my-courses/{course_id}/groups/",
+    "/apps/teachers/me/",
+    "/apps/teachers/list/",
+    "/apps/teachers/create/",
+    "/apps/teachers/detail/{pk}/",
+    "/apps/teachers/{pk}/toggle-archive/",
+    "/apps/teachers/{pk}/upload-image/",
+    "/apps/teachers/delete/{pk}/",
+    "/apps/teachers/update/{pk}/",
 }
 
 
