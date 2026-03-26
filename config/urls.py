7eaedb3cfb3fb229f38ef0252db02e28c78d0079
Maxsud Toshpulat <maxsudtoshpulat@Maxsuds-MacBook-Air.local>
2026-03-26 17:08:31 +0500

    a

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -32,3 +33,6 @@ urlpatterns += [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('apps/', include('apps.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
