aeae6a02237056e57f1e93e6fd87e118e16cd753
Maxsud Toshpulat <maxsudtoshpulat@Maxsuds-MacBook-Air.local>
2026-03-31 10:21:54 +0500

    a

--- config/settings/base.py
+++ config/settings/base.py
@@ -182,7 +182,7 @@ CELERY_BEAT_SCHEDULE = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
