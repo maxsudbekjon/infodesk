76ba30eb07906a4fccb4110075ea60e8ccf1f4b0
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-04-01 09:52:00 +0500

    updated

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,9 +1,15 @@
 
 import os
+import sys
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
-django_env = os.environ.get("DJANGO_ENV", "dev").lower()
+django_env = os.environ.get("DJANGO_ENV", "base").lower()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{django_env}")
 
 application = get_wsgi_application()
+
+
+logging.info("WSGI started, django settings is %s", django_env)
+
